0ec8a6bed6cc1185f2ced1901eba46bf31fbce13
Benji <97198967+BenPI88@users.noreply.github.com>
2023-08-15 14:00:35 -0500

    Update backup.py

--- backup.py
+++ backup.py
@@ -1,5 +1,12 @@
+import platform
 temp = open("version.ini")
 version = temp.read()
 temp.close()
 print(f"BenPI88 File Backup Tool {version}")
-print("Checking for updates...")
+if str(platform.system()) == "Windows":
+  os = "Windows"
+elif str(platform.system()) == "Darwin":
+  os = "MacOS"
+else
+  os = "Linux"
+print(f"Detected OS: {os} ({platform.version()})")

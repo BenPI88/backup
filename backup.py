dfff02f29d65d3516e717d3926c11f14a4daa757
Benji <97198967+BenPI88@users.noreply.github.com>
2023-08-15 14:14:43 -0500

    Update backup.py

--- backup.py
+++ backup.py
@@ -9,4 +9,10 @@ elif str(platform.system()) == "Darwin":
   os = "MacOS"
 else:
   os = "Linux"
-print(f"Detected OS: {os}")
+print(f"Detected OS: {os}\n")
+
+#shitty code to check if the option is invalid or not
+option = "0"
+while not str(option) == "1" or str(option) == "2" or str(option) == "3":
+print("Please select an option.\n1) Backup\n2) Restore\n3) Exit")
+option = input()
